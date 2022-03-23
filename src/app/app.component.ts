d1ed74247e9734421cdb962dfffee2ba14a9c27a
kiliamMartin <kmarting99@gmail.com>
2022-03-23 23:04:56 +0000

    finished calculator app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculator';
+  title = 'Simple Calculator';
+  screen="";
+  operator=""
+
+
+  show(element:any){
+    this.screen += element.toString()
+  }
+
+
+  resolve(){
+    var value = eval(this.screen)
+    this.screen = (value).toString()
+
+  }
+
+  reset(){
+    this.screen = ""
+  }
+
+  setOperator(operator:string):void {
+    this.operator = operator
+    this.show(operator)
+  }
+
+  
+
 }
